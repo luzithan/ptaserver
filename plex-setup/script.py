1005e9203c3380dfd057b0e2ed6e5e6461c66dd5
luzithan <luzithan@gmail.com>
2018-05-21 21:24:39 +0200

    fix plex-setup

--- plex-setup/script.py
+++ plex-setup/script.py
@@ -14,28 +14,34 @@ print('plex auth' + plex_auth)
 plex = PlexServer(plex_server, plex_auth)
 
 movie_section = AttrDict(scanner="Plex Movie Scanner", language="en", type="movie", agent="com.plexapp.agents.imdb",
-                         location="/movies", name="Movies")
+                         locations=["/movies"], title="Movies")
 
 movies_data_section = AttrDict(scanner="Plex Movie Scanner", language="en", type="movie", agent="com.plexapp.agents.imdb",
-                         location="/data/movies", name="Data Movies")
+                         locations=["/data/movies"], title="Data Movies")
 
 tv_section = AttrDict(scanner="Plex Series Scanner", language="en", type="show", agent="com.plexapp.agents.thetvdb",
-                      location="/tv", name="TV Shows")
+                      locations=["/tv"], title="TV Shows")
 
 tv_data_section = AttrDict(scanner="Plex Movie Scanner", language="en", type="movie", agent="com.plexapp.agents.imdb",
-                         location="/data/tv", name="Data TV Shows")
+                         locations=["/data/tv"], title="Data TV Shows")
 
 sections_to_add = [movie_section, tv_section]
 
 for section_to_add in sections_to_add:
     found = False
+    print('to_add: ' + str(list(section_to_add.items())))
     for section in plex.library.sections():
         second = vars(section)
         # section_to_add found completely inside secod
+        print('present: ' + str(list(second.items())))
         if all(k in second and second[k] == v for k, v in section_to_add.items()):
             # found the same section
             found = True
             print('Found section already added %s' % section_to_add)
+    print('---------------')
     if not found:
-        print('Add section %s' % section_to_add)
-        plex.library.add(**section_to_add)
+        print('Addin Section %s' % section_to_add.title)
+#        plex.library.add(**section_to_add)
+    else:
+        print('Section %s already present' % section_to_add.title)
+
